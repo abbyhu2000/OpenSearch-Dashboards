f8fd82657537843687c5f46bfd5f022574604791
abbyhu2000 <abigailhu2000@gmail.com>
2022-09-08 21:07:12 +0000

    should

--- src/core/public/chrome/ui/header/header_logo.tsx
+++ src/core/public/chrome/ui/header/header_logo.tsx
@@ -108,7 +108,6 @@ interface Props {
 }
 
 export function HeaderLogo({ href, navigateToApp, branding, ...observables }: Props) {
-  console.log("test workflow!")
   const forceNavigation = useObservable(observables.forceNavigation$, false);
   const navLinks = useObservable(observables.navLinks$, []);
   const {
